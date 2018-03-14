c55658972cdb7f304503c9cbe5e93894efa2cd8a
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-03-15 01:43:21 +0300

    Update to 2.7.4

--- cmake/pypi_process.py
+++ cmake/pypi_process.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# {
-#     "info": {
-#         "maintainer": null,
-#         "docs_url": "http://pythonhosted.org/psycopg2/",
-#         "requires_python": null,
-#         "maintainer_email": null,
-#         "cheesecake_code_kwalitee_id": null,
-#         "keywords": null,
-#         "package_url": "http://pypi.python.org/pypi/psycopg2",
-#         "author": "Federico Di Gregorio",
-#         "author_email": "fog@initd.org",
-#         "download_url": "http://initd.org/psycopg/tarballs/PSYCOPG-2-6/psycopg2-2.6.2.tar.gz",
-#         "platform": "any",
-#         "version": "2.6.2",
-
 import sys
 import os
 import json
@@ -27,12 +12,20 @@ with open(sys.argv[1]) as data_file:
     data = json.load(data_file)
 
     version = data['info']['version']
-    download_url = data['info']['download_url']
-    date = data['releases'][version][0]['upload_time'].replace('T', ' ')
+    name = data['info']['name']
+    for i in data['releases'][version]:
+        if i['url'].endswith('tar.gz') or i['url'].endswith('zip'):
+            download_url = i['url']
+            date = i['upload_time'].replace('T', ' ')
+            break
+
+#    download_url = data['info']['download_url']
+#    date = data['releases'][version][0]['upload_time'].replace('T', ' ')
 
     version_file_name = os.path.join(os.path.dirname(sys.argv[1]), 'version.str')
     version_file = open(version_file_name, 'w')
-    version_file.write("%s\n%s" % (version, date))
+    pack_name = "{}-{}-{}".format(name, version, sys.argv[2])
+    version_file.write("{}\n{}\n{}".format(version, date, pack_name))
     version_file.close()
 
-print download_url + ';' + version
+print download_url + ';' + version + ';' + pack_name
